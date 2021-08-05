9a607f8d761cfc842345f4ddf8cf16a78569d675
Michael Humphris <hello@michaelhumphris.com>
2021-08-05 08:45:10 -0700

    Make left encoder undo/redo

--- keyboards/keyhive/sofle/rev1/encoder.c
+++ keyboards/keyhive/sofle/rev1/encoder.c
@@ -1,18 +1,18 @@
  /* Copyright 2020 Josef Adamcik
   * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
-  * 
-  * This program is free software: you can redistribute it and/or modify 
-  * it under the terms of the GNU General Public License as published by 
-  * the Free Software Foundation, either version 2 of the License, or 
-  * (at your option) any later version. 
-  * 
-  * This program is distributed in the hope that it will be useful, 
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-  * GNU General Public License for more details. 
-  * 
-  * You should have received a copy of the GNU General Public License 
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
   */
 
 //Setting up what encoder rotation does. If your encoder can be pressed as a button, that function can be set in Via.
@@ -28,9 +28,12 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_PGDOWN);
+            register_mods(mod_config(MOD_LCTL));
+            tap_code(KC_Z);
         } else {
-            tap_code(KC_PGUP);
+            register_mods(mod_config(MOD_LCTL) | MOD_LSFT);
+            tap_code(KC_Z);
+            unregister_mods(mod_config(MOD_LCTL) | MOD_LSFT);
         }
     }
 }
